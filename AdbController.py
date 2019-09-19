3e4d6117f8b639444367908c2ff90a25af9a2561
unknown <panxw@funshion.com>
2019-09-19 15:51:26 +0800

    update ui

--- AdbController.py
+++ AdbController.py
@@ -12,12 +12,12 @@ from tkinter import messagebox
 print("窗口启动...")
 
 root = tk.Tk()
-root.geometry("180x260")
+root.geometry("200x268")
 iptextvar = tk.Variable() 
 iptextvar.set('172.16.13.106')
 
 resptextvar=tk.Variable()
-resptextvar.set('...')
+resptextvar.set('请连接.')
 
 isConnected = False
 
@@ -97,14 +97,14 @@ def execRebootCmd():
 
 
 header= tk.Frame(root)
-home=tk.Button(header, text ="首页", command =lambda:exeCmdKey('3'))
+home=tk.Button(header, text ="首页", font='bold',command =lambda:exeCmdKey('3'))
 home.pack(side='left')
-menu=tk.Button(header, text ="菜单", command = lambda:exeCmdKey('1'))
+menu=tk.Button(header, text ="菜单", font='bold',command = lambda:exeCmdKey('1'))
 menu.pack(side='left')
-setting=tk.Button(header, text ="设置", command =lambda:exeAction('bestv.ott.action.setting.sys'))
+setting=tk.Button(header, text ="设置", font='bold',command =lambda:exeAction('bestv.ott.action.setting.sys'))
 setting.pack(side='left')
-back=tk.Button(header, text ="返回", font='bold', fg='orange',command =lambda: exeCmdKey('4'))
-back.pack(side='left')
+reboot=tk.Button(header, text ="重启",font='bold', command = lambda:execRebootCmd())
+reboot.pack(side='left')
 header.pack(side='top')
 
 
@@ -119,49 +119,55 @@ right=tk.Button(grid, text ="右",font='bold', width=5, command = lambda:exeCmdK
 right.grid(row=2, column=2)
 down=tk.Button(grid, text ="下",font='bold', width=5, command = lambda:exeCmdKey('20'))
 down.grid(row=3, column=1)
+back=tk.Button(grid, text ="返回", font='bold', fg='orange',command =lambda: exeCmdKey('4'))
+back.grid(row=3, column=2)
+
+
 grid.pack(side='top')
 
 addr= tk.Frame(root)
-ip = tk.Label(addr,text = "地址:")
+ip = tk.Label(addr,text = "地址:",font='bold',)
 ip.pack(side='left')
 inputip=tk.Entry(addr,textvariable=iptextvar,width=15)
 inputip.pack(side='left')
-connect = tk.Button(addr,text="连接",command=lambda:execConnect())
+connect = tk.Button(addr,text="连接",font='bold',command=lambda:execConnect())
 connect.pack(side='left')
 addr.pack(side='top')
 
 funs=tk.Frame(root)
-disconnect = tk.Button(funs,text="断开",command=lambda:execDisConnect())
+disconnect = tk.Button(funs,text="断开",font='bold',command=lambda:execDisConnect())
 disconnect.pack(side='right')
-reboot=tk.Button(funs, text ="重启", command = lambda:execRebootCmd())
-reboot.pack(side='right')
 funs.pack(side='top')
 
 more=tk.Frame(root)
-zhibo=tk.Button(more, text ="直播", command =lambda:exeAction('bestv.ott.action.web --es param SERVICE_H5JINGXUAN'))
+zhibo=tk.Button(more, text ="直播",font='bold', command =lambda:exeAction('bestv.ott.action.web --es param SERVICE_H5JINGXUAN'))
 zhibo.grid(row=0, column=0)
-dianbo=tk.Button(more, text ="点播", command = lambda:exeAction('bestv.ott.action.categories'))
+dianbo=tk.Button(more, text ="点播",font='bold', command = lambda:exeAction('bestv.ott.action.categories'))
 dianbo.grid(row=0, column=1)
-yingyong=tk.Button(more, text ="应用", command =lambda: exeAction('bestv.ott.action.appstore'))
+yingyong=tk.Button(more, text ="应用",font='bold', command =lambda: exeAction('bestv.ott.action.appstore'))
 yingyong.grid(row=0, column=2)
-favor=tk.Button(more, text ="收藏", command =lambda: exeAction('bestv.ott.action.favorite'))
+favor=tk.Button(more, text ="收藏", font='bold',command =lambda: exeAction('bestv.ott.action.favorite'))
 favor.grid(row=0, column=3)
 more.pack(side='top')
 
 volumn=tk.Frame(root)
-mute=tk.Button(volumn, text ="mute", command =lambda:exeCmdKey('164'))
+mute=tk.Button(volumn, text ="mute", font='bold',command =lambda:exeCmdKey('164'))
 mute.pack(side='left')
-zhibo=tk.Button(volumn, text ="vol+", command =lambda:exeCmdKey('24'))
+zhibo=tk.Button(volumn, text ="vol+", font='bold',command =lambda:exeCmdKey('24'))
 zhibo.pack(side='left')
-dianbo=tk.Button(volumn, text ="vol-", command = lambda:exeCmdKey('25'))
+dianbo=tk.Button(volumn, text ="vol-", font='bold',command = lambda:exeCmdKey('25'))
 dianbo.pack(side='left')
 volumn.pack(side='top')
 
 
 alertinfo=tk.Frame(root)
-informtext=ip = tk.Label(alertinfo,textvariable=resptextvar,fg='gray')
+informtext=ip = tk.Label(alertinfo,textvariable=resptextvar,fg='red',font=('Times New Roman',10))
 informtext.pack(side='left')
 alertinfo.pack(side='top')
 
+versionFrame=tk.Frame(root);
+versionInfo = tk.Label(versionFrame,text='v1.0_panxw',fg='gray',font=('Times New Roman',8))
+versionInfo.pack(side='right')
+versionFrame.pack(side='top')
 
 root.mainloop()# 进入消息循环
